68122e10805bece9d2673298dc87619741375c37
Sergii Kushnir <skush@users.noreply.github.com>
2017-10-23 15:42:46 +0300

    ProductService - hard coded

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -22,32 +23,9 @@ export class ProductListComponent {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023-32",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        // this.filteredProducts = this.products;
-        this.listFilter = 'cart'
+    constructor(private _productService: ProductService) {
     }
 
     onRatingClicked(message: string): void {
@@ -64,9 +42,9 @@ export class ProductListComponent {
         this.showImage = !this.showImage;
     }
 
-    /*
-    ngOnInit(): void {
-        console.log('In Init');
+     ngOnInit(): void {
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
-    */
+
 }
