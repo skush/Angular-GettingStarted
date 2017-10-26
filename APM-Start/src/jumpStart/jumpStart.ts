c9f552b18939dc099f7c7ae7cba03dd747c64ac7
Sergii Kushnir <skush@users.noreply.github.com>
2017-10-26 06:45:58 +0300

    jump start notes

---
+++ APM-Start/src/jumpStart/jumpStart.ts
@@ -0,0 +1,21 @@
+//to delete:
+// created for taking notes =at Jump Start
+ng new lesson-planner
+ng generate component ...__dirname
+
+
+file '.angular-cli.json':
+prefix: "pm" ("app")
+
+<nav>
+    <a> Lesson Planneer </a>
+</nav>
+
+const routes: Routes = [
+    { path: "lessons", component: LessonsListComponent }
+];
+
+<a roterLink="settings" roterLinkActive="selected-menu-item">
+
+//правильно:
+// нужно чтобі компоненты были тегами, а деректовы - атрибутами
